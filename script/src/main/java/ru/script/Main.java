7d4f14a44eeb342770752282ab4abf5afcb177d0
nikisha-script <nikisha_script@mail.ru>
2022-08-18 08:57:55 +0300

    #solid.md

---
+++ script/src/main/java/ru/script/Main.java
@@ -0,0 +1,47 @@
+package ru.script;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(args[0])));
+             PrintWriter writer = new PrintWriter(new BufferedOutputStream(new FileOutputStream(args[1])))
+        ) {
+            StringBuilder sb = new StringBuilder();
+            List<String> firstList = new ArrayList<>();
+            List<String> secondList = new ArrayList<>();
+            int i = 1;
+            for (String read = reader.readLine(); read != null; read = reader.readLine()) {
+                sb.append(read);
+                sb.insert(0, i++ + ")" + " " + "[");
+                sb.insert(sb.length(), "]" + "(#" + read
+                                .replace(" ", "-")
+                                .replace("?", "")
+                                .replace(".", "")
+                                .replace(",", "")
+                                .replace("()", "")
+                                .toLowerCase(Locale.ROOT)
+                        + ")");
+                firstList.add(sb.toString());
+                sb = new StringBuilder();
+                sb.append("## ").append(read
+                        .replace("?", "")
+                        .replace(".", ""));
+                secondList.add(sb.toString());
+                sb = new StringBuilder();
+            }
+            firstList.addAll(secondList);
+            writer.println("## " + args[1] + "\n");
+            firstList.forEach(writer::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
